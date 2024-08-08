f9311ad6f59426a725142948a762170373fa8d40
shiv325 <139581888+shiv325@users.noreply.github.com>
2024-08-08 21:42:22 +0530

    Added Files

---
+++ script.js
@@ -0,0 +1,39 @@
+function updatePHValue(value) {
+    document.getElementById("pH-Val").innerText = value;
+}
+
+function updateTurbidityValue(value) {
+    document.getElementById("turbidity-Value").innerText = value;
+}
+
+function updateSalinityValue(value) {
+    document.getElementById("salinity-Value").innerText = value;
+}
+
+function updateTempValue(value) {
+    document.getElementById("temperature-Value").innerText = value;
+}
+
+function updateTempValue(value) {
+    document.getElementById("temperature-Value").innerText = value;
+}
+
+function updateTempValue(value) {
+    document.getElementById("temperature-Value").innerText = value;
+}
+
+function updateTempValue(value) {
+    document.getElementById("temperature-Value").innerText = value;
+}
+
+function updateTempValue(value) {
+    document.getElementById("temperature-Value").innerText = value;
+}
+
+function updateTempValue(value) {
+    document.getElementById("temperature-Value").innerText = value;
+}
+
+function updateTempValue(value) {
+    document.getElementById("temperature-Value").innerText = value;
+}
