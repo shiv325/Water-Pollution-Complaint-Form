12e4b6dadce27329a20582fa19d99818b9490c69
shiv325 <139581888+shiv325@users.noreply.github.com>
2024-08-10 18:43:07 +0530

    Modified Files

--- script.js
+++ script.js
@@ -14,26 +14,30 @@ function updateTempValue(value) {
     document.getElementById("temperature-Value").innerText = value;
 }
 
-function updateTempValue(value) {
-    document.getElementById("temperature-Value").innerText = value;
+function updateTDSValue(value) {
+    document.getElementById("tds-Value").innerText = value;
 }
 
-function updateTempValue(value) {
-    document.getElementById("temperature-Value").innerText = value;
+function updateOxygenValue(value) {
+    document.getElementById("oxygen-Value").innerText = value;
 }
 
-function updateTempValue(value) {
-    document.getElementById("temperature-Value").innerText = value;
+function updateHardnessValue(value) {
+    document.getElementById("hardness-Value").innerText = value;
 }
 
-function updateTempValue(value) {
-    document.getElementById("temperature-Value").innerText = value;
+function updateChlorideValue(value) {
+    document.getElementById("chloride-Value").innerText = value;
 }
 
-function updateTempValue(value) {
-    document.getElementById("temperature-Value").innerText = value;
+function updateColiformBacteriaValue(value) {
+    document.getElementById("coliform-bacteria-Value").innerText = value;
 }
 
-function updateTempValue(value) {
-    document.getElementById("temperature-Value").innerText = value;
+function updateAlgaeValue(value) {
+    document.getElementById("algae-Value").innerText = value;
+}
+
+function updateAmmoniaValue(value) {
+    document.getElementById("ammonia-Value").innerText = value;
 }
